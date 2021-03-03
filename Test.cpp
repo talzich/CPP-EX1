3f2612f624333f0d41cae5364e0359caba5366af
Tal <talzhtc@gmail.com>
2021-03-03 11:16:14 +0200

    New file

---
+++ Test.cpp
@@ -0,0 +1,27 @@
+/**
+ * AUTHORS: Tal Zichlinsky
+ * 
+ * Date: 2021-03
+ */
+
+
+#include "doctest.h"
+#include "snowman.hpp"
+using namespace ariel;
+
+#include <string>
+using namespace std;
+
+
+TEST_CASE("Good snowman code") {
+    CHECK(snowman(11114411) == string("_===_\n(.,.)\n( : )\n( : )"));
+    /* Add more checks here */
+}
+
+TEST_CASE("Bad snowman code") {
+    CHECK_THROWS(snowman(555));
+    /* Add more checks here */
+}
+
+
+/* Add more test cases here */
